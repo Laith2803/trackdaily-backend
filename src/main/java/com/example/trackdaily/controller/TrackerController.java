94280301ab5ff073f67d7b19b7ff3cdb3b0f0917
Laith Alabtah <laith.alabtah@htw-berlin.de>
2026-05-24 20:55:54 +0200

    allow forntend access with CORS

--- src/main/java/com/example/trackdaily/controller/TrackerController.java
+++ src/main/java/com/example/trackdaily/controller/TrackerController.java
@@ -1,14 +1,15 @@
 package com.example.trackdaily.controller;
 
 import com.example.trackdaily.model.Tracker;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TrackerController {
-
     @GetMapping("/api/trackers")
     public List<Tracker> getTrackers() {
         return List.of(
